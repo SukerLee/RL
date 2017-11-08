2668390590d66d0f323a96a06f93f49c5111f5c8
Suker <suker040609@gmail.com>
2017-11-08 10:50:31 +0800

    no message

--- ptt_scale/run.py
+++ ptt_scale/run.py
@@ -5,23 +5,23 @@ import re
 
 # 目標頁面
 
-res = requests.get('https://www.ptt.cc/bbs/Beauty/index2300.html')
+res = requests.get('https://www.ptt.cc/bbs/Beauty/index2109.html')
 soup = BeautifulSoup(res.text, 'lxml')
 
 # 使用迴圈進入到目標頁面中的每個主題頁面
 
 for article in soup.select('.r-ent a'):
-    url = 'https://www.ptt.cc' + article['h_url['href'], article.text, index))ref']
+    url = 'https://www.ptt.cc' + article['href']
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'lxml')
 
     # 判斷網址中有沒有圖片，如果有就開始下載
 
-    if len(soup.findAll('a', {'href': re.compile('https:\/\/i\.imgur\.com\/.*')})) > 0:
-        for index, img_url in enumerate(soup.findAll('a', {'href': re.compile('https:\/\/i\.imgur\.com\/.*')})):
+    if len(soup.findAll('a', {'href': re.compile('http:\/\/i\.imgur\.com\/.*')})) > 0:
+        for index, img_url in enumerate(soup.findAll('a', {'href': re.compile('http:\/\/i\.imgur\.com\/.*')})):
             try:
                 # 記得更改想要下載到的位置
-                print(article.text)
-                urlretrieve(img_url['href'], 'E:\py_project\dl\{}_{}.jpg'.format(article.text, index))
+
+                urlretrieve(img_url['href'], 'D:\{}_{}.jpg'.format(article.text, index))
             except:
-                print('{} {}_{}.jpg 下載失敗!'.format(img
+                print('{} {}_{}.jpg 下載失敗!'.format(img_url['href'], article.text, index))
